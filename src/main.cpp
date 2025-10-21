b3300c8c829308bfeeaef6130b737544c9fc2272
EgyipTomi425 <tomi04252002@gmail.com>
2025-10-21 19:01:52 +0200

    Using QT app

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@ import echterwachter;
 
 #include <cstdio>
 #include <iostream>
+#include <thread>
+
+#include <QCoreApplication>
+#include <QTimer>
+#include <QDebug>
+#include <chrono>
 
 int main()
 {
@@ -15,6 +21,15 @@ int main()
 #endif
 
     std::cout << "MAGIC_NUMBER: " << magic_number << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    qtAdd([]()
+    {
+        QTimer* timer = new QTimer(qt.app);
+        QObject::connect(timer, &QTimer::timeout, [](){ qDebug() << "Timer tick"; });
+        timer->start(1000);
+    });
 
     // Change it true if you have new commands
     start_bot(false); // If you use it too often, you will reach the rate limit
