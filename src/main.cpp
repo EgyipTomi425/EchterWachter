5f346153ce2b849965adff17bcf31106c0d20d82
EgyipTomi425 <tomi04252002@gmail.com>
2025-12-03 02:53:12 +0100

    Adding DC support to statistics module

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,13 @@ int main()
 #endif
 #endif
 
+#ifdef UTIL
+    std::cout << "[UTIL]: ON " << "42" << std::endl;
+#ifdef STATISTICS
+    std::cout << "[STATISTICS]: ON " << init_statistics << std::endl;
+#endif
+#endif
+
     std::cout << "MAGIC_NUMBER: " << magic_number << std::endl;
 
     start_bot(false); // If you use it too often, you will reach the rate limit
